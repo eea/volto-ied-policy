564a578e2695c936147004e677ba457daccb185b
eea-jenkins <eea-github@googlegroups.com>
2025-04-16 14:51:55 +0200

    style: Automated code fix

--- src/components/manage/Blocks/FiltersMap/View.jsx
+++ src/components/manage/Blocks/FiltersMap/View.jsx
@@ -65,7 +65,13 @@ const View = ({ data, providers_data, query, dispatch }) => {
         });
       }
     }
-  }, [data, providers_data, filtersInitialized, permitTypes, setInitialFilters]);
+  }, [
+    data,
+    providers_data,
+    filtersInitialized,
+    permitTypes,
+    setInitialFilters,
+  ]);
 
   useEffect(() => {
     if (providers_data !== prevProvidersData.current) {
