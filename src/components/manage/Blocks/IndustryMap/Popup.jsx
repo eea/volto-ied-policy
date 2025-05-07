e4cc488765e3d16319a1bad74072ea1a9d8dc716
Teodor <teodorvoicu87@gmail.com>
2025-05-07 13:25:56 +0300

    make Popup component functional instead of class based

--- src/components/manage/Blocks/IndustryMap/Popup.jsx
+++ src/components/manage/Blocks/IndustryMap/Popup.jsx
@@ -1,79 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from 'semantic-ui-react';
 
-class Popup extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.onPointermove = this.onPointermove.bind(this);
-    this.state = {
-      data: {},
+const Popup = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (JSON.stringify(e.detail) !== JSON.stringify(data)) {
+        setData(e.detail);
+      }
     };
-  }
 
-  onPointermove(e) {
-    if (JSON.stringify(e.detail) !== JSON.stringify(this.state.data)) {
-      this.setState({ data: e.detail });
-    }
-  }
+    const mapElement = document.querySelector('#industry-map');
+    mapElement?.addEventListener('ol-pointermove', handler);
 
-  componentDidMount() {
-    document
-      .querySelector('#industry-map')
-      ?.addEventListener('ol-pointermove', this.onPointermove);
-  }
+    return () => {
+      mapElement?.removeEventListener('ol-pointermove', handler);
+    };
+  }, [data]);
 
-  componentWillUnmount() {
-    document
-      .querySelector('#industry-map')
-      ?.removeEventListener('ol-pointermove', this.onPointermove);
-  }
-
-  render() {
-    const { data } = this.state;
-    return (
-      <div
-        id="popup"
-        style={
-          !Object.keys(data).length
-            ? { display: 'none', pointerEvents: 'auto' }
-            : {}
-        }
-      >
-        <div className="popover-header">
-          {data.siteName ? (
-            <h3>{data.siteName}</h3>
-          ) : data.NUTS_NAME && data.CNTR_CODE && data.COUNTRY ? (
-            <h3>{`${data.NUTS_NAME}, ${data.CNTR_CODE}, ${data.COUNTRY}`}</h3>
+  return (
+    <div
+      id="popup"
+      style={
+        !Object.keys(data).length
+          ? { display: 'none', pointerEvents: 'auto' }
+          : {}
+      }
+    >
+      <div className="popover-header">
+        {data.siteName ? (
+          <h3>{data.siteName}</h3>
+        ) : data.NUTS_NAME && data.CNTR_CODE && data.COUNTRY ? (
+          <h3>{`${data.NUTS_NAME}, ${data.CNTR_CODE}, ${data.COUNTRY}`}</h3>
+        ) : (
+          ''
+        )}
+      </div>
+      <div className="popover-body">
+        <Grid.Column stretched>
+          {data.num_sites ? (
+            <Grid.Row>
+              <p>
+                Number of sites: <code>{data.num_sites}</code>
+              </p>
+            </Grid.Row>
           ) : (
             ''
           )}
-        </div>
-        <div className="popover-body">
-          <Grid.Column stretched>
-            {data.num_sites ? (
-              <Grid.Row>
-                <p>
-                  Number of sites: <code>{data.num_sites}</code>
-                </p>
-              </Grid.Row>
+          <Grid.Row>
+            {data.hdms ? (
+              <>
+                <p className="mb-1">The location you are viewing is:</p>
+                <code>{data.hdms}</code>
+              </>
             ) : (
               ''
             )}
-            <Grid.Row>
-              {data.hdms ? (
-                <>
-                  <p className="mb-1">The location you are viewing is:</p>
-                  <code>{data.hdms}</code>
-                </>
-              ) : (
-                ''
-              )}
-            </Grid.Row>
-          </Grid.Column>
-        </div>
+          </Grid.Row>
+        </Grid.Column>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Popup;
