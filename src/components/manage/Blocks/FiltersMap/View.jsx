26988da2f28527db0377e4179a1c0d183124f6c6
Miu Razvan <miu.razvan28@gmail.com>
2026-03-27 11:20:52 +0200

    Fix site details page, ref #301941

--- src/components/manage/Blocks/FiltersMap/View.jsx
+++ src/components/manage/Blocks/FiltersMap/View.jsx
@@ -215,22 +215,6 @@ const View = ({
     updateOptions();
   }, [updateOptions]);
 
-  //Reset query state and clean URL when component unmounts
-  useEffect(() => {
-    const explorePath = location.pathname;
-    return () => {
-      dispatch(resetQuery());
-      setFiltersInitialized(false);
-      // Clean URL params when leaving the page
-      setTimeout(() => {
-        if (!window.location.pathname.includes(explorePath)) {
-          const cleanUrl = window.location.pathname;
-          window.history.replaceState({}, '', cleanUrl);
-        }
-      }, 0);
-    };
-  }, [dispatch, location.pathname]);
-
   return (
     <div className="filters-block outline-button">
       <Search data={data} providers_data={providers_data} />
